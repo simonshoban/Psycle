be5a62184f4e9c1b4c8f92d30f497a32d8dd2b37
Simon Shoban <simonshoban@gmail.com>
2018-10-29 20:05:22 -0700

    Move button listener logic to a separate function; delete unused import statement; rename onClick function override parameter

--- app/src/main/java/ca/kelownakangaroos/psycle/MainActivity.java
+++ app/src/main/java/ca/kelownakangaroos/psycle/MainActivity.java
@@ -3,7 +3,6 @@ package ca.kelownakangaroos.psycle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,11 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setButtonOnClickListeners();
+    }
+
+    private void setButtonOnClickListeners() {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View viewElement) {
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-                Button button = (Button) v;
+                Button button = (Button) viewElement;
 
                 intent.putExtra("Title", button.getText());
 
